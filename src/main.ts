b0cd8717592af748660c181ffe58c621062dda79
Ruben Blanco <r87bnbm@gmail.com>
2024-11-15 13:52:29 +0100

    TS

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,1 @@
 //node .\src\main.js para ejecutar el archivo en Node.js y salga en consola
-console.log((120-18)/6); //Restamos el precio de las bebidas al total y dividimos entre los comensales.
-
